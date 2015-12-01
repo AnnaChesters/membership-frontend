7ae95e9ab1a6f2978220708483a9fda62f0e8ef7
Thomas Franquelin <ostapneko@gmail.com>
2015-12-01 16:02:38 +0000

    Fix minor front end bugs in the checkout

--- frontend/app/views/support/countryWithCurrency.scala
+++ frontend/app/views/support/countryWithCurrency.scala
@@ -7,7 +7,9 @@ case class CountryWithCurrency(country: i18n.Country, currency: i18n.Currency)
 
 object CountryWithCurrency {
   val all = i18n.CountryGroup.allGroups.flatMap(group =>
-    group.countries.map(c => CountryWithCurrency(c, group.currency)))
+    group.countries
+      .map(c => CountryWithCurrency(c, group.currency)))
+      .sortBy(_.country.name)
 
   def withCurrency(c: Currency) = all.map(_.copy(currency = c))
 
