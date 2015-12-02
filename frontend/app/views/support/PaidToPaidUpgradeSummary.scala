0ffab42adf18d53f04d99b97a724caf2ece6abfa
Andrea Fiore <andrea.fiore@composable.io>
2015-12-02 16:49:20 +0000

    Simplify next payment date logic in updgrade previews

--- frontend/app/views/support/PaidToPaidUpgradeSummary.scala
+++ frontend/app/views/support/PaidToPaidUpgradeSummary.scala
@@ -1,9 +1,9 @@
 package views.support
 
-import com.gu.membership.model.{BillingPeriod, Current, PaidTierPlan, Price}
+import com.github.nscala_time.time.OrderingImplicits.DateTimeOrdering
+import com.gu.membership.model._
 import com.gu.membership.salesforce.PaidTier
 import com.gu.membership.stripe.Stripe.Card
-import com.gu.membership.zuora.rest
 import com.gu.membership.zuora.soap.models.Queries.PreviewInvoiceItem
 import model.MembershipCatalog
 import org.joda.time.{DateTime, LocalDate}
@@ -18,18 +18,21 @@ case class PaidToPaidUpgradeSummary(billingPeriod: BillingPeriod, current: Curre
 
 object PaidToPaidUpgradeSummary {
   case class UpgradeSummaryError(subNumber: String, targetTier: PaidTier)(msg: String) extends Throwable {
-    override def getMessage = s"Failure while trying to display an upgrade summary for the subscription ${subNumber} to $targetTier: $msg"
+    override def getMessage = s"Failure while trying to display an upgrade summary for the subscription $subNumber to $targetTier: $msg"
   }
 
   def apply(catalog: MembershipCatalog, invoices: Seq[PreviewInvoiceItem], sub: model.PaidSubscription, targetTier: PaidTier, card: Card): PaidToPaidUpgradeSummary = {
     val upgradeError = UpgradeSummaryError(sub.number, targetTier) _
     val accountCurrency = sub.accountCurrency
 
-    val sortedInvoiceItems = invoices.sortBy(_.price)
-    val futureSubscriptionInvoice = sortedInvoiceItems.lastOption.getOrElse(
-      throw upgradeError("Expected at least one invoice item in the preview")
-    )
-    val firstPayment = Price(invoices.map(_.price).sum.toInt, accountCurrency)
+    // The sorted invoice items list will include as its first element a refund with a negative
+    // price. We add up the refund to the next invoice item, thus computing a pro-rated price for the upgrade.
+    val firstPayment = invoices.sortBy(_.price) match {
+      case refundItem :: targetPlanItem :: _ if refundItem.price < 0 =>
+        Price((refundItem.price + targetPlanItem.price).toInt, accountCurrency)
+      case _ => throw new IllegalStateException(
+        s"Failed to compute a pro-rated price from invoice items $invoices. Subscription: ${sub.number}, target tier: $targetTier")
+    }
 
     val billingPeriod = sub.plan.billingPeriod
     val targetTierPlan = PaidTierPlan(targetTier, billingPeriod, Current)
@@ -52,7 +55,10 @@ object PaidToPaidUpgradeSummary {
         tier = targetTier,
         firstPayment =  firstPayment,
         nextPayment = targetPrice,
-        nextPaymentDate = futureSubscriptionInvoice.serviceStartDate.toLocalDate
+        nextPaymentDate = billingPeriod match {
+          case Year => LocalDate.now().plusYears(1)
+          case Month => LocalDate.now().plusMonths(1)
+        }
       )
 
     PaidToPaidUpgradeSummary(billingPeriod, currentSummary, targetSummary)
